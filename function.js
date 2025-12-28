8abb069599fd2d5944c45b1d91b5dc61cb6758e7
Fida-ur-Rahman <fidasaba15@gmail.com>
2025-12-28 23:31:19 +0500

    MY JavaScript Assignment No 13

---
+++ function.js
@@ -0,0 +1,24 @@
+// FUNCTION IN JAVASCRIPT 
+// PROGRAM 1
+function fun(){
+    console.log("Fida ur Rahman")
+    console.log("RAHMAN")
+}
+fun();
+
+// PROGRAM 2
+function sum(x,y){
+    console.log(x+y)
+    console.log(x-y)
+    console.log(x*y)
+    console.log(x/y)
+    console.log(x%y)
+}
+sum(10,10);
+
+// ARROW FUNCTION IN JAVASCRIPT
+const arrowsum = (a, b)=>{
+    return a+b;
+};
+arrowsum(1, 3)
+
